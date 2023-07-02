ab311a45c3ae48bb118582c07d382b6a7d8449ed
Jonatan Holmgren <jonatan@jontes.page>
2023-07-02 12:05:55 +0200

    Introduce identity

---
+++ src/stores.ts
@@ -0,0 +1,24 @@
+import { writable } from "svelte/store";
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+// Create a writable store that is initialized with data from a JWT in localStorage that may or may not exist
+export const user = writable<User>({
+  firstName: "",
+  lastName: "",
+  email: "",
+});
+
+if (localStorage.getItem("supersecrettoken")) {
+  const token = localStorage.getItem("supersecrettoken");
+  const payload = JSON.parse(
+    atob(token.split(".")[1].replace("-", "+").replace("_", "/"))
+  );
+  user.set({
+    firstName: payload.firstName,
+    lastName: payload.lastName,
+    email: payload.email,
+  });
+}
